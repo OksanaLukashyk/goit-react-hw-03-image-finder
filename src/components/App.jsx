b597d8478497890820354c074de16563407376d1
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-11-06 17:49:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
-import Modal from './Modal/Modal';
+import { Modal } from './Modal/Modal';
 
 export class App extends Component {
 
