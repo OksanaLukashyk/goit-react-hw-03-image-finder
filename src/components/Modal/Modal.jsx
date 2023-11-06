297121a590ca51760eaf247d88015e4d8a4aca62
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-11-06 15:05:58 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,12 +4,12 @@ import css from './Modal.module.css';
 export default class Modal extends Component {
     componentDidMount() {
         window.addEventListener('keydown', this.closeOnEsc);
-        document.getElementsByTagName("body") = 'hidden';
+        document.getElementsByTagName("body")[0].style.overflow = 'hidden';
     }
 
     componentWillUnmount() {
         window.removeEventListener('keydown', this.closeOnEsc);
-        document.getElementsByTagName("body") = 'auto';
+        document.getElementsByTagName("body")[0].style.overflow = 'auto';
     }
 
     closeOnOverlayClick = (evt) => {
